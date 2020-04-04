5ca974bd01567146a48d2576cd993705b628fd22
kokejang <63136302+kokejang@users.noreply.github.com>
2020-04-04 13:09:36 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,12 @@
 import discord
+import os
 import datetime
 client=discord.Client()
-token='Njk1NjUxOTE5NDA2MDM5MDcy.XofALA.DirFQqjK4a4L1C6SGw-z8Jd_UFM'
 @client.event
 async def on_ready():
     print('----------------------------------------------------------------------------')
     print(f'| Client ID: {client.user.id}                                             |')
     print(f'| Client Name: {client.user.name}                                                    |')
-    print(f'| Client Token: {str(token)} |')
     print('----------------------------------------------------------------------------\n\n=========================================\n\n')
 @client.event
 async def on_message(message):
@@ -20,5 +19,7 @@ async def on_message(message):
                 await message.channel.send('DM을 확인해주세요!')
         except:
             print('에러가 발생하였습니다')
-client.run(token)
+                                          
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
 
